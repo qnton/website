82e1ffd98aac80ee73b1a90b3e14054a7062ab13
qnton <hello@qnt.one>
2023-07-30 12:14:43 +0200

    fixing 404

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,8 +8,10 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://qnt.one/",
   integrations: [react(), tailwind(), sitemap(), robotsTxt()],
-  output: "server",
-  adapter: cloudflare(),
+  output: "hybrid",
+  adapter: cloudflare({
+    mode: "directory",
+  }),
   experimental: {
     assets: true,
   },
