f5c32711a972690ce2677b9aee612276141b81b7
qnton <hello@qnt.one>
2023-08-04 22:36:21 +0200

    add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
   site: "https://qnt.one/",
   integrations: [tailwind(), sitemap(), robotsTxt()],
   output: "static",
+  compressHTML: true,
   trailingSlash: "never",
   experimental: {
     assets: true,
@@ -14,6 +15,32 @@ export default defineConfig({
   image: {
     service: {
       entrypoint: "./src/image-service.ts",
+    },
+  },
+  redirects: {
+    "/github": {
+      status: 302,
+      destination: "https://github.com/qnton",
+    },
+    "/linkedin": {
+      status: 302,
+      destination: "https://www.linkedin.com/in/qnton",
+    },
+    "/mail": {
+      status: 302,
+      destination: "mailto:hello@qnt.one",
+    },
+    "/project/schnelltest": {
+      status: 302,
+      destination: "https://schnelltest-clp.de",
+    },
+    "/project/hm-medizintechnik": {
+      status: 302,
+      destination: "https://hm-medizintechnik.de",
+    },
+    "/project/hankens": {
+      status: 302,
+      destination: "https://hankens-testzentrum.de",
     },
   },
 });
