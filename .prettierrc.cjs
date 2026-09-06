909d850aad1496fc9b75612e690e14c0467172b6
qnton <hello@qnt.one>
2026-09-06 03:27:03 +0200

    rework

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,27 +3,5 @@ module.exports = {
     require.resolve("prettier-plugin-astro"),
     require.resolve("prettier-plugin-tailwindcss"),
   ],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
-  singleAttributePerLine: true,
-  quoteProps: "as-needed",
-  bracketSameLine: false,
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: true,
-  singleQuote: false,
-  trailingComma: "all",
-  bracketSpacing: true,
-  jsxSingleQuote: false,
-  arrowParens: "always",
-  proseWrap: "never",
-  htmlWhitespaceSensitivity: "strict",
-  endOfLine: "lf",
+  overrides: [{ files: "*.astro", options: { parser: "astro" } }],
 };
